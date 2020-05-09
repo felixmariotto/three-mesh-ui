2d92c9703b733f162692f21fa7c45ee5ce29de98
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-05-09 15:47:30 +0200

    remove error

--- examples/js/utils/VRControl.js
+++ examples/js/utils/VRControl.js
@@ -80,8 +80,6 @@ export default function VRControl( renderer, camera, scene ) {
 	pointer.scale.set(0.015, 0.015, 1)
 	pointer.renderOrder = 1000;
 
-	fre = ger
-
 	////////////////
 	// Controllers
 	////////////////
