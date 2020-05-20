d90c395b5837113b6974a7b3603215d99c913a9f
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-05-20 11:16:51 +0200

    organise 'content' folder

---
+++ src/content/glyphs/GeometryGlyph.js
@@ -0,0 +1,12 @@
+/*
+
+Job: create a text geometry of the wanted glyph.
+Knows: dimension of the rectangle containing the glyph
+
+*/
+
+export default function GeometryGlyph() {
+
+	console.log('hello from GeometryGlyph');
+
+};
---
+++ src/content/glyphs/MSDFGlyph.js
@@ -0,0 +1,12 @@
+/*
+
+Job: create a plane geometry with the right UVs to map the MSDF texture on the wanted glyph.
+Knows: dimension of the plane to create
+
+*/
+
+export default function MSDFGlyph() {
+
+	console.log('hello from MSDFGlyph');
+
+};
