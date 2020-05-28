d821c6c13e963a61e73f98d4a8dcde9973c4708a
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-05-28 12:58:59 +0200

    organise fr keyMap differently

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -43,7 +43,7 @@ export default function KeyboardModule( options ) {
 		switch ( navigator.language ) {
 
 			case 'fr' :
-				keymap = keymaps.ru
+				keymap = keymaps.fr
 				break
 
 			default :
--- src/utils/Keymaps.js
+++ src/utils/Keymaps.js
@@ -14,113 +14,113 @@ export default {
 
 		[
 
-			{
-				a: { width: 0.1, upperCase: "A" },
-				z: { width: 0.1, upperCase: "Z" },
-				e: { width: 0.1, upperCase: "E" },
-				r: { width: 0.1, upperCase: "R" },
-				t: { width: 0.1, upperCase: "T" },
-				y: { width: 0.1, upperCase: "Y" },
-				u: { width: 0.1, upperCase: "U" },
-				i: { width: 0.1, upperCase: "I" },
-				o: { width: 0.1, upperCase: "O" },
-				p: { width: 0.1, upperCase: "P" }
-			},
+			[
+				{ width: 0.1, chars: [{ lowerCase: "a", upperCase: "A" }] },
+				{ width: 0.1, chars: [{ lowerCase: "z", upperCase: "Z" }] },
+				{ width: 0.1, chars: [{ lowerCase: "e", upperCase: "E" }] },
+				{ width: 0.1, chars: [{ lowerCase: "r", upperCase: "R" }] },
+				{ width: 0.1, chars: [{ lowerCase: "t", upperCase: "T" }] },
+				{ width: 0.1, chars: [{ lowerCase: "y", upperCase: "Y" }] },
+				{ width: 0.1, chars: [{ lowerCase: "u", upperCase: "U" }] },
+				{ width: 0.1, chars: [{ lowerCase: "i", upperCase: "I" }] },
+				{ width: 0.1, chars: [{ lowerCase: "o", upperCase: "O" }] },
+				{ width: 0.1, chars: [{ lowerCase: "p", upperCase: "P" }] }
+			],
 
-			{
-				q: { width: 0.1, upperCase: "Q" },
-				s: { width: 0.1, upperCase: "S" },
-				d: { width: 0.1, upperCase: "D" },
-				f: { width: 0.1, upperCase: "F" },
-				g: { width: 0.1, upperCase: "G" },
-				h: { width: 0.1, upperCase: "H" },
-				j: { width: 0.1, upperCase: "J" },
-				k: { width: 0.1, upperCase: "K" },
-				l: { width: 0.1, upperCase: "L" },
-				m: { width: 0.1, upperCase: "M" }
-			},
+			[
+				{ width: 0.1, chars: [{ lowerCase: "q", upperCase: "Q" }] },
+				{ width: 0.1, chars: [{ lowerCase: "s", upperCase: "S" }] },
+				{ width: 0.1, chars: [{ lowerCase: "d", upperCase: "D" }] },
+				{ width: 0.1, chars: [{ lowerCase: "f", upperCase: "F" }] },
+				{ width: 0.1, chars: [{ lowerCase: "g", upperCase: "G" }] },
+				{ width: 0.1, chars: [{ lowerCase: "h", upperCase: "H" }] },
+				{ width: 0.1, chars: [{ lowerCase: "j", upperCase: "J" }] },
+				{ width: 0.1, chars: [{ lowerCase: "k", upperCase: "K" }] },
+				{ width: 0.1, chars: [{ lowerCase: "l", upperCase: "L" }] },
+				{ width: 0.1, chars: [{ lowerCase: "m", upperCase: "M" }] }
+			],
 
-			{
-				shift: { width: 0.2, command: 'shift' },
-				w: { width: 0.1, upperCase: "W" },
-				x: { width: 0.1, upperCase: "X" },
-				c: { width: 0.1, upperCase: "C" },
-				v: { width: 0.1, upperCase: "V" },
-				b: { width: 0.1, upperCase: "B" },
-				n: { width: 0.1, upperCase: "N" },
-				cancel: { width: 0.2, command: 'backspace' }
-			},
+			[
+				{ width: 0.2, command: 'shift', chars: [{ icon: "shift" }] },
+				{ width: 0.1, chars: [{ lowerCase: "w", upperCase: "W" }] },
+				{ width: 0.1, chars: [{ lowerCase: "x", upperCase: "X" }] },
+				{ width: 0.1, chars: [{ lowerCase: "c", upperCase: "C" }] },
+				{ width: 0.1, chars: [{ lowerCase: "v", upperCase: "V" }] },
+				{ width: 0.1, chars: [{ lowerCase: "b", upperCase: "B" }] },
+				{ width: 0.1, chars: [{ lowerCase: "n", upperCase: "N" }] },
+				{ width: 0.2, command: 'backspace', chars: [{ icon: "cancel" }] }
+			],
 
-			{
-				'.?12': { width: 0.2, command: 'switch' },
-				',': { width: 0.1 },
-				space: { width: 0.4, command: 'space' },
-				'.': { width: 0.1 },
-				enter: { width: 0.2, command: 'enter' }
-			}
+			[
+				{ width: 0.2, command: 'switch', chars: [{ lowerCase: ".?12" }] },
+				{ width: 0.1, chars: [{ lowerCase: "," }] },
+				{ width: 0.4, command: 'space', chars: [{ icon: "space" }] },
+				{ width: 0.1, chars: [{ lowerCase: "." }] },
+				{ width: 0.2, command: 'enter', chars: [{ icon: "enter" }] }
+			]
 
 		],
 
 		[
 
-			{
-				'1': { width: 0.1 },
-				'2': { width: 0.1 },
-				'3': { width: 0.1 },
-				'4': { width: 0.1 },
-				'5': { width: 0.1 },
-				'6': { width: 0.1 },
-				'7': { width: 0.1 },
-				'8': { width: 0.1 },
-				'9': { width: 0.1 },
-				'0': { width: 0.1 }
-			},
+			[
+				{ width: 0.1, chars: [{ lowerCase: "1" }] },
+				{ width: 0.1, chars: [{ lowerCase: "2" }] },
+				{ width: 0.1, chars: [{ lowerCase: "3" }] },
+				{ width: 0.1, chars: [{ lowerCase: "4" }] },
+				{ width: 0.1, chars: [{ lowerCase: "5" }] },
+				{ width: 0.1, chars: [{ lowerCase: "6" }] },
+				{ width: 0.1, chars: [{ lowerCase: "7" }] },
+				{ width: 0.1, chars: [{ lowerCase: "8" }] },
+				{ width: 0.1, chars: [{ lowerCase: "9" }] },
+				{ width: 0.1, chars: [{ lowerCase: "0" }] }
+			],
 
-			{
-				'@': { width: 0.1 },
-				'#': { width: 0.1 },
-				'|': { width: 0.1 },
-				'_': { width: 0.1 },
-				'&': { width: 0.1 },
-				'-': { width: 0.1 },
-				'+': { width: 0.1 },
-				'(': { width: 0.1 },
-				')': { width: 0.1 },
-				'/': { width: 0.1 },
-			},
+			[
+				{ width: 0.1, chars: [{ lowerCase: "@" }] },
+				{ width: 0.1, chars: [{ lowerCase: "#" }] },
+				{ width: 0.1, chars: [{ lowerCase: "|" }] },
+				{ width: 0.1, chars: [{ lowerCase: "_" }] },
+				{ width: 0.1, chars: [{ lowerCase: "&" }] },
+				{ width: 0.1, chars: [{ lowerCase: "-" }] },
+				{ width: 0.1, chars: [{ lowerCase: "+" }] },
+				{ width: 0.1, chars: [{ lowerCase: "(" }] },
+				{ width: 0.1, chars: [{ lowerCase: ")" }] },
+				{ width: 0.1, chars: [{ lowerCase: "/" }] }
+			],
 
-			{
-				'=': { width: 0.1 },
-				'*': { width: 0.1 },
-				'"': { width: 0.1 },
-				"'": { width: 0.1 },
-				':': { width: 0.1 },
-				';': { width: 0.1 },
-				'!': { width: 0.1 },
-				'?': { width: 0.1 },
-				backspace: { width: 0.2, command: 'backspace' }
-			},
+			[
+				{ width: 0.1, chars: [{ lowerCase: "=" }] },
+				{ width: 0.1, chars: [{ lowerCase: "*" }] },
+				{ width: 0.1, chars: [{ lowerCase: '"' }] },
+				{ width: 0.1, chars: [{ lowerCase: "'" }] },
+				{ width: 0.1, chars: [{ lowerCase: ":" }] },
+				{ width: 0.1, chars: [{ lowerCase: ";" }] },
+				{ width: 0.1, chars: [{ lowerCase: "!" }] },
+				{ width: 0.1, chars: [{ lowerCase: "?" }] },
+				{ width: 0.2, command: 'backspace', chars: [{ icon: "backspace" }] }
+			],
 
-			{
-				'.?12': { width: 0.2, command: 'switch' },
-				',': { width: 0.1 },
-				space: { width: 0.4, command: 'space' },
-				'.': { width: 0.1 },
-				enter: { width: 0.2, command: 'enter' }
-			}
+			[
+				{ width: 0.2, command: 'switch', chars: [{ lowerCase: ".?12" }] },
+				{ width: 0.1, chars: [{ lowerCase: "," }] },
+				{ width: 0.4, command: 'space', chars: [{ icon: "space" }] },
+				{ width: 0.1, chars: [{ lowerCase: "." }] },
+				{ width: 0.2, command: 'enter', chars: [{ icon: "enter" }] }
+			]
 
 		]
 
 	],
 
 	///////////////////////////////////////////////////////////
-
+	/*
 	eng:
 	[
 
 		[
 
-			{
+			[
 				q: { width: 0.1, upperCase: "Q" },
 				w: { width: 0.1, upperCase: "W" },
 				e: { width: 0.1, upperCase: "E" },
@@ -131,9 +131,9 @@ export default {
 				i: { width: 0.1, upperCase: "I" },
 				o: { width: 0.1, upperCase: "O" },
 				p: { width: 0.1, upperCase: "P" }
-			},
+			],
 
-			{
+			[
 				a: { width: 0.1, upperCase: "A" },
 				s: { width: 0.1, upperCase: "S" },
 				d: { width: 0.1, upperCase: "D" },
@@ -143,9 +143,9 @@ export default {
 				j: { width: 0.1, upperCase: "J" },
 				k: { width: 0.1, upperCase: "K" },
 				l: { width: 0.1, upperCase: "L" }
-			},
+			],
 
-			{
+			[
 				shift: { width: 0.15, command: 'shift' },
 				z: { width: 0.1, upperCase: "Z" },
 				x: { width: 0.1, upperCase: "X" },
@@ -155,21 +155,21 @@ export default {
 				n: { width: 0.1, upperCase: "N" },
 				m: { width: 0.1, upperCase: "M" },
 				cancel: { width: 0.15, command: 'backspace' }
-			},
+			],
 
-			{
+			[
 				'.?12': { width: 0.2, command: 'switch' },
 				',': { width: 0.1 },
 				space: { width: 0.4, command: 'space' },
 				'.': { width: 0.1 },
 				enter: { width: 0.2, command: 'enter' }
-			}
+			]
 
 		],
 
 		[
 
-			{
+			[
 				'1': { width: 0.1 },
 				'2': { width: 0.1 },
 				'3': { width: 0.1 },
@@ -180,9 +180,9 @@ export default {
 				'8': { width: 0.1 },
 				'9': { width: 0.1 },
 				'0': { width: 0.1 }
-			},
+			],
 
-			{
+			[
 				'@': { width: 0.1 },
 				'#': { width: 0.1 },
 				'|': { width: 0.1 },
@@ -193,9 +193,9 @@ export default {
 				'(': { width: 0.1 },
 				')': { width: 0.1 },
 				'/': { width: 0.1 },
-			},
+			],
 
-			{
+			[
 				'=': { width: 0.1 },
 				'*': { width: 0.1 },
 				'"': { width: 0.1 },
@@ -205,15 +205,15 @@ export default {
 				'!': { width: 0.1 },
 				'?': { width: 0.1 },
 				backspace: { width: 0.2, command: 'backspace' }
-			},
+			],
 
-			{
+			[
 				'.?12': { width: 0.2, command: 'switch' },
 				',': { width: 0.1 },
 				space: { width: 0.4, command: 'space' },
 				'.': { width: 0.1 },
 				enter: { width: 0.2, command: 'enter' }
-			}
+			]
 
 		]
 
@@ -224,7 +224,7 @@ export default {
 
 		[
 
-			{
+			[
 				й: { width: 1/12, upperCase: "Й" },
 				ц: { width: 1/12, upperCase: "Ц" },
 				у: { width: 1/12, upperCase: "У" },
@@ -237,9 +237,9 @@ export default {
 				э: { width: 1/12, upperCase: "Э" },
 				х: { width: 1/12, upperCase: "Х" },
 				ъ: { width: 1/12, upperCase: "Ъ" }
-			},
+			],
 
-			{
+			[
 				ф: { width: 1/12, upperCase: "Ф" },
 				ы: { width: 1/12, upperCase: "Ы" },
 				в: { width: 1/12, upperCase: "В" },
@@ -252,9 +252,9 @@ export default {
 				ж: { width: 1/12, upperCase: "Ж" },
 				з: { width: 1/12, upperCase: "З" },
 				ё: { width: 1/12, upperCase: "Ё" }
-			},
+			],
 
-			{
+			[
 				shift: { width: 1.5/12, command: 'shift' },
 				я: { width: 1/12, upperCase: "Я" },
 				ч: { width: 1/12, upperCase: "Ч" },
@@ -266,21 +266,21 @@ export default {
 				б: { width: 1/12, upperCase: "Б" },
 				ю: { width: 1/12, upperCase: "Ю" },
 				cancel: { width: 1.5/12, command: 'backspace' }
-			},
+			],
 
-			{
+			[
 				'eng': { width: 0.15, command: 'switch-set' },
 				'.?12': { width: 0.15, command: 'switch' },
 				space: { width: 0.4, command: 'space' },
 				'.': { width: 0.1 },
 				enter: { width: 0.2, command: 'enter' }
-			}
+			]
 
 		],
 
 		[
 
-			{
+			[
 				'1': { width: 0.1 },
 				'2': { width: 0.1 },
 				'3': { width: 0.1 },
@@ -291,9 +291,9 @@ export default {
 				'8': { width: 0.1 },
 				'9': { width: 0.1 },
 				'0': { width: 0.1 }
-			},
+			],
 
-			{
+			[
 				'@': { width: 0.1 },
 				'#': { width: 0.1 },
 				'|': { width: 0.1 },
@@ -304,9 +304,9 @@ export default {
 				'(': { width: 0.1 },
 				')': { width: 0.1 },
 				'/': { width: 0.1 },
-			},
+			],
 
-			{
+			[
 				'=': { width: 0.1 },
 				'*': { width: 0.1 },
 				'"': { width: 0.1 },
@@ -316,17 +316,18 @@ export default {
 				'!': { width: 0.1 },
 				'?': { width: 0.1 },
 				backspace: { width: 0.2, command: 'backspace' }
-			},
+			],
 
-			{
+			[
 				'АБВ': { width: 0.3, command: 'switch' },
 				space: { width: 0.4, command: 'space' },
 				'.': { width: 0.1 },
 				enter: { width: 0.2, command: 'enter' }
-			}
+			]
 
 		]
 
 	]
+	*/
 
 }
