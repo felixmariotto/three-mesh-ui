5661f2786bf4ef090879bce01f5f6c2507fd5e3a
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-05-31 20:10:44 +0200

    load SVG for inlineImage (still not right scale)

--- src/content/ImageManager.js
+++ src/content/ImageManager.js
@@ -19,17 +19,25 @@ import Frame from './Frame.js';
 
 // TEMP
 
-import { MeshBasicMaterial } from 'three';
+import { MeshBasicMaterial, Group, Color, ShapeBufferGeometry } from 'three';
+
+// to delete
 import { SphereBufferGeometry, MeshNormalMaterial } from 'three';
 
 //
 
 const textureLoader = new TextureLoader();
+const svgLoader = new SVGLoader();
 
 const loadingMaterial = new MeshBasicMaterial({
 	transparent: true,
 	opacity: 0
 });
+
+const DRAW_FILL_SHAPE = true;
+const DRAW_STROKE = true;
+const FILL_SHAPE_WIREFRAME = false;
+const STROKE_WIREFRAME = false;
 
 //
 
@@ -54,8 +62,7 @@ function create( options ) {
 			return makeTexturedPlane( options, fileExtension );
 
 		case 'svg' :
-			console.log('create svg');
-			break
+			return makeSVG( options );
 
 		default :
 			console.warn(`extension of file ${options.src} is not supported`);
@@ -101,5 +108,93 @@ function makeTexturedPlane( options, fileExtension ) {
 	});
 
 	return mesh
+
+};
+
+//
+
+function makeSVG( options ) {
+
+	const container = new Group();
+
+	svgLoader.load( options.src, function ( data ) {
+
+		var paths = data.paths;
+
+		var group = new Group();
+		group.scale.multiplyScalar( 0.25 );
+		group.position.x = - 70;
+		group.position.y = 70;
+		group.scale.y *= - 1;
+
+		for ( var i = 0; i < paths.length; i ++ ) {
+
+			var path = paths[ i ];
+
+			var fillColor = path.userData.style.fill;
+			if ( DRAW_FILL_SHAPE && fillColor !== undefined && fillColor !== 'none' ) {
+
+				var material = new MeshBasicMaterial( {
+					color: new Color().setStyle( fillColor ),
+					opacity: path.userData.style.fillOpacity,
+					transparent: path.userData.style.fillOpacity < 1,
+					side: 2,
+					depthWrite: false,
+					wireframe: FILL_SHAPE_WIREFRAME
+				});
+
+				var shapes = path.toShapes( true );
+
+				for ( var j = 0; j < shapes.length; j ++ ) {
+
+					var shape = shapes[ j ];
+
+					var geometry = new ShapeBufferGeometry( shape );
+					var mesh = new Mesh( geometry, material );
+
+					group.add( mesh );
+
+				};
+
+			};
+
+			var strokeColor = path.userData.style.stroke;
+
+			if ( DRAW_STROKE && strokeColor !== undefined && strokeColor !== 'none' ) {
+
+				var material = new MeshBasicMaterial( {
+					color: new Color().setStyle( strokeColor ),
+					opacity: path.userData.style.strokeOpacity,
+					transparent: path.userData.style.strokeOpacity < 1,
+					side: 2,
+					depthWrite: false,
+					wireframe: STROKE_WIREFRAME
+				});
+
+				for ( var j = 0, jl = path.subPaths.length; j < jl; j ++ ) {
+
+					var subPath = path.subPaths[ j ];
+
+					var geometry = SVGLoader.pointsToStroke( subPath.getPoints(), path.userData.style );
+
+					if ( geometry ) {
+
+						var mesh = new Mesh( geometry, material );
+
+						group.add( mesh );
+
+					};
+
+				};
+
+			};
+
+		};
+
+		container.add( group );
+
+	});
+
+	return container
 
 };
