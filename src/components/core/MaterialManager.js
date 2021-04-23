3a944fd08e300830fa33f55fdbd7b12c0b1abd7a
Felix Mariotto <felixmariotto@gmail.com>
2021-04-23 19:08:06 +0200

    MaterialManager: reduce z-offset of text material to fix issue with ortho camera

--- src/components/core/MaterialManager.js
+++ src/components/core/MaterialManager.js
@@ -219,7 +219,7 @@ const textVertex = `
 		vUv = uv;
 		vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
 		gl_Position = projectionMatrix * mvPosition;
-		gl_Position.z-= 0.005;
+		gl_Position.z -= 0.00001;
 
 		#include <clipping_planes_vertex>
 
