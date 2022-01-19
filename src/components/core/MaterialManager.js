3176ff5b014592d86d77edc72598b527d0ce0be2
Felix Herbst <herbst@prefrontalcortex.de>
2022-01-19 11:12:28 +0100

    add Rotated Grid Super Sampling
    based on https://bgolus.medium.com/sharper-mipmapping-using-shader-based-supersampling-ed7aadb47bec

--- src/components/core/MaterialManager.js
+++ src/components/core/MaterialManager.js
@@ -228,6 +228,9 @@ export default function MaterialManager( Base = class {} ) {
                 fragmentShader: textFragment,
                 extensions: {
                     derivatives: true
+                },
+                defines: {
+                    USE_RGSS: true,
                 }
             })
 
@@ -329,7 +332,34 @@ const textFragment = `
     }
 
     void main() {
+#ifdef USE_RGSS
+        // shader-based supersampling based on https://bgolus.medium.com/sharper-mipmapping-using-shader-based-supersampling-ed7aadb47bec
+        // per pixel partial derivatives
+        vec2 dx = dFdx(vUv);
+        vec2 dy = dFdy(vUv);
+
+        // rotated grid uv offsets
+        vec2 uvOffsets = vec2(0.125, 0.375);
+        float _Bias = -0.5;
+        vec2 offsetUV = vec2(0.0, 0.0);
+
+        // supersampled using 2x2 rotated grid
+        vec4 col = vec4(0.0);
+        offsetUV.xy = vUv + uvOffsets.x * dx + uvOffsets.y * dy;
+        col += texture(u_texture, offsetUV, _Bias);
+        offsetUV.xy = vUv - uvOffsets.x * dx - uvOffsets.y * dy;
+        col += texture(u_texture, offsetUV, _Bias);
+        offsetUV.xy = vUv + uvOffsets.y * dx - uvOffsets.x * dy;
+        col += texture(u_texture, offsetUV, _Bias);
+        offsetUV.xy = vUv - uvOffsets.y * dx + uvOffsets.x * dy;
+        col += texture(u_texture, offsetUV, _Bias);
+        col *= 0.25;
+
+        vec3 msd = col.rgb;
+#else
         vec3 msd = texture( u_texture, vUv ).rgb;
+#endif
+
         float sd = median(msd.r, msd.g, msd.b);
         float screenPxDistance = screenPxRange()*(sd - 0.5);
         float alpha = clamp(screenPxDistance + 0.5, 0.0, 1.0);
