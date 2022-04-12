16b4b28cd1500922609e18064c925c6e4a185602
th_swingtree <th@swingtree.be>
2022-04-12 16:21:09 +0200

    build both exported library js and module. Add also minified build version

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,24 +1,81 @@
-const webpack = require('webpack');
+'use strict';
 
-module.exports = env => {
+const ESLintPlugin = require('eslint-webpack-plugin');
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
-	return {
+// As we will exports both module and js build, start a base configuration
+const baseConfig = {
+	mode: 'production',
 
-		mode: 'production',
+	plugins:[
+		new ESLintPlugin( { overrideConfigFile: './config/codestyle/.eslintrc', })
+	],
 
-		entry: {
-			'../build/three-mesh-ui': './src/three-mesh-ui.js',
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin( {
+				test: /\.js(\?.*)?$/i,
+
+				// only minimize .min.js files
+				include: /\.min\.js$/,
+				extractComments: 'some',
+				terserOptions: {
+					format: {
+						comments: /@license/i,
+					},
+					compress: {
+
+						// remove console.logs while leaving other console outputs
+						pure_funcs: [ 'console.log' ],
+					},
+				}
+			} ),
+		],
+	},
+
+	// Do not export threejs
+	externals: {
+		three: 'three',
+	},
+};
+
+
+const moduleConfig = {
+	target: 'node',
+
+	// 2 files, raw + min
+	entry: {
+		'../build/three-mesh-ui.module': './src/three-mesh-ui.js',
+		'../build/three-mesh-ui.module.min': './src/three-mesh-ui.js',
+	},
+
+	// as this configuration use `output.library.type='module'`
+	experiments: {
+		outputModule: true,
+	},
+
+	output: {
+		filename: '[name].js', // force .js instead of .mjs
+		chunkFormat: 'module',
+		library: {
+			type: 'module',
 		},
+	},
 
-		output: {
-			filename: '[name].js',
-			library: 'ThreeMeshUI',
-			libraryTarget: 'umd'
-		},
-		externals: {
-			three:'THREE'
-		}
+	...baseConfig
+};
 
-	}
+const browserConfig = {
+	target: 'web',
 
-}
+	entry: {
+		'../build/three-mesh-ui': './src/three-mesh-ui.js',
+		'../build/three-mesh-ui.min': './src/three-mesh-ui.js',
+	},
+
+	...baseConfig
+};
+
+// Export both module and browser config ( ... browser is wrongly named )
+module.exports = [ moduleConfig, browserConfig ];
