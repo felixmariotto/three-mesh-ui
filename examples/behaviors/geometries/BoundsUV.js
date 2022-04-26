eccd8b7172854ce9d739ef023efdcc6822e8700a
th_swingtree <th@swingtree.be>
2022-04-26 12:57:43 +0200

    First behavior wip

---
+++ examples/behaviors/geometries/BoundsUV.js
@@ -0,0 +1,19 @@
+export default class BoundsUV {
+
+	/**
+	 *
+	 * @param {MeshUIComponent} target
+	 * @param {MeshUIComponent} reference
+	 * @param {"uv","uv2","uvG"} uvSet
+	 */
+	constructor( target, reference, uvSet = 'uv') {
+
+		reference.onAfterUpdate = function() {
+
+			
+
+		}
+
+	}
+
+}
